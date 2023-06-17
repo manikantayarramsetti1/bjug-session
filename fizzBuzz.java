17591608959b279c6ee7e74368f56ced2f4ea8d5
manikanta <manikanta@dataworkz.io>
2023-06-17 19:28:35 +0530

    day1

---
+++ fizzBuzz.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class fizzBuzz {
+    public static void main(String[] args) {
+        for(int num = 1; num <= 50; num++) {
+            if (num % 3== 0 && num % 5 ==0) {
+                System.out.println("FizzBuzz");
+            }else if(num % 3 == 0) {
+                System.out.println("Fizz");
+            } else if(num % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(num);
+            }
+        }
+    }
+}
